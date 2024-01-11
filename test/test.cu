d0ad9d523df4e309a23f7755ad4fe197b2d0c57e
Steef Hegeman <s.hegeman@liacs.leidenuniv.nl>
2024-01-12 00:39:46 +0100

    extra info about doctest

--- test/test.cu
+++ test/test.cu
@@ -1,3 +1,6 @@
+// Tests are written with the implementation (in the "include" directory).
+// This file instructs doctest to gather them and generate an entrypoint.
+
 #define DOCTEST_CONFIG_USE_STD_HEADERS
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
