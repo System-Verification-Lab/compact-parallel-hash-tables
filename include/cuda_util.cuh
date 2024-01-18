50c775f757f056b8d418baf024645dabbea027b8
Steef Hegeman <s.hegeman@liacs.leidenuniv.nl>
2024-01-18 16:40:20 +0100

    helper to invoke __device__ function as kernel
    nvcc does not allow __global__ functions to be a member of a class,
    while __device__ functions can be member functions. So this basically
    allows for writing kernels as member __device__ void functions.

--- include/cuda_util.cuh
+++ include/cuda_util.cuh
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cuda_runtime.h>
+#include <functional>
 
 void cuda_assert(cudaError_t code, const char *file, const int line) {
 	if (code == cudaSuccess) return;
@@ -10,3 +11,12 @@ void cuda_assert(cudaError_t code, const char *file, const int line) {
 }
 
 #define CUDA(check) cuda_assert((check), __FILE__, __LINE__)
+
+// A kernel that calls __device__ function F with given arguments
+//
+// As __global__ functions cannot be member functions and __device__ functions
+// can, this can be used to "wrap" a member __device__ function in a kernel.
+template <auto F, class... Args>
+__global__ void invoke_device(Args... args) {
+	std::invoke(F, args...);
+}
