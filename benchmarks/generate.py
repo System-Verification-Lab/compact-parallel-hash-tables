a2b83eb5d96b5532960bc91c6990cec01a79f3ff
Steef Hegeman <s.hegeman@liacs.leidenuniv.nl>
2024-01-21 23:59:41 +0100

    benchmarks: bit more realistic data set
    Still needs more work (multiple data sets), but at least now the number
    of duplicates is not incredibly high.

--- benchmarks/generate.py
+++ benchmarks/generate.py
@@ -2,7 +2,9 @@ import sys
 import numpy as np
 
 SEED = 2024_01_02_17_25_20  # time of initial commit
+MIN = 0
+MAX = 2**45-1
 
 rng = np.random.default_rng(SEED)
-keys = rng.integers(0, 100, 20_000_000)
+keys = rng.integers(0, MAX + 1, 20_000_000)
 np.savetxt(sys.stdout, keys, fmt="%d")
