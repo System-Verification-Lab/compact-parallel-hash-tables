12a02a03a90f174f8ae81358051e5df850d3773a
Steef Hegeman <s.hegeman@liacs.leidenuniv.nl>
2024-02-27 19:13:48 +0100

    note to phase out kh namespace

--- include/table.cuh
+++ include/table.cuh
@@ -5,6 +5,7 @@ enum class Result { FOUND, PUT, FULL };
 
 // Helpers for directing kernels to device member functions
 // (because __global__ functions cannot be member functions)
+// TODO: this is to be phased out in favor of invoke_device in cuda_util.cuh
 namespace kh {
 	template <class Table>
 	__global__ void find(Table *table, const key_type *start, const key_type *end, bool *results) {
