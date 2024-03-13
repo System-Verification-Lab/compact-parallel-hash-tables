d9be6f9911ea66afb45bff27c1ce81a4238f0701
Steef Hegeman <s.hegeman@liacs.leidenuniv.nl>
2024-03-13 12:45:11 +0100

    benchmarks/generate.py: not pretty but works

--- benchmarks/generate.py
+++ benchmarks/generate.py
@@ -1,11 +1,6 @@
 import numpy as np
 
 SEED = 2024_01_02_17_25_20  # time of initial commit
-
 rng = np.random.default_rng(SEED)
-keys = rng.integers(0, 2**45, 20_000_000, dtype=np.uint64)
-keys.tofile("benchmarks/data/1.bin")
-
-rng = np.random.default_rng(SEED)
-keys = rng.integers(0, 2**13, 20_000_000, dtype=np.uint64)
-keys.tofile("benchmarks/data/dups.bin")
+keys = rng.choice(2**39, 1208_000_000, replace=False).astype(np.uint64)
+keys.tofile("/data/hegemans/1208m39.bin")
